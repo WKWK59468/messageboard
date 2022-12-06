cb27a90daac274883f1b68cc6ee096fe21278b90
jrong <wkwk59468@gmail.com>
2022-12-06 14:54:57 +0800

    getAllMessage Controller Success

--- controllers/message.controller.php
+++ controllers/message.controller.php
@@ -1,3 +1,11 @@
 <?php
-    // $acc = $_SESSION["acc"];
+    require_once("./models/sql.model.php");
+    class MessageController {
+        public function getAllMessage(){
+            $MessagesRepository = new MessagesRepository();
+            $messages = $MessagesRepository->getAllMessages();
+            
+            return $messages;
+        }
+    }
 ?>
